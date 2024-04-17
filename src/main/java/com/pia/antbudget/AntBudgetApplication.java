6b4c1b68b018df0f794a2dd93ad859c2fc54ac8f
natalia <natsil220119@gmail.com>
2024-04-17 17:27:45 -0500

    proyecto base de antBudget

---
+++ src/main/java/com/pia/antbudget/AntBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.pia.antbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntBudgetApplication.class, args);
+	}
+
+}
