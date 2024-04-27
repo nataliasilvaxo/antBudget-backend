6a4cedfba86521ee1440148b0882b0a76519f6a2
natalia <natsil220119@gmail.com>
2024-04-27 17:13:35 -0500

    servicio guardar categoria

--- src/main/java/com/pia/antbudget/controller/CategoryRestController.java
+++ src/main/java/com/pia/antbudget/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pia.antbudget.model.Category;
 import com.pia.antbudget.response.CategoryResponseRest;
 import com.pia.antbudget.services.ICategoryService;
 
@@ -30,6 +33,15 @@ public class CategoryRestController {
 	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
 		
 		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
+	}
+	
+	//guardar las categorias
+	@PostMapping("/categories")
+	//en el parametro de entrada con el requestBody podemos recuperar lo que venga del cliente como objeto JSON
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+		
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
 		return response;
 	}
 }
--- src/main/java/com/pia/antbudget/services/ICategoryService.java
+++ src/main/java/com/pia/antbudget/services/ICategoryService.java
@@ -2,11 +2,13 @@ package com.pia.antbudget.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.pia.antbudget.model.Category;
 import com.pia.antbudget.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 	
 }
